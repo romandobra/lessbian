a1b9a1f5374f553826f7695910da3ed713576451
undecoded <67537080+undecoded@users.noreply.github.com>
2021-03-31 21:13:42 +0700

    symplify autostart

--- lessbian-hooks.sh
+++ lessbian-hooks.sh
@@ -4,7 +4,11 @@ Type=Application
 Exec=lessbian-autostart
 Hidden=false
 X-GNOME-Autostart-enabled=true
-Terminal=true
 Name=lessbian-autostart' > /home/${LIVE_USERNAME}/.config/autostart/lessbian-autostart.desktop
-
+echo '. go' > /home/${LIVE_USERNAME}/.bash_history
 chown -R ${LIVE_USERNAME} /home/${LIVE_USERNAME}
+
+echo "gsettings set org.gnome.desktop.background picture-options none
+gnome-terminal --tab -e \"echo HELLO\"
+" > /usr/bin/lessbian-autostart
+chmod +x /usr/bin/lessbian-autostart
