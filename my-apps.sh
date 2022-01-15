8691001ca219ee277eb697ad076de08b730774be
romandobra <67537080+romandobra@users.noreply.github.com>
2022-01-15 18:11:43 +0000

    Delete my-apps.sh
    Going 2.0

--- my-apps.sh
+++
@@ -1,5 +0,0 @@
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-#apt -y install ./google-chrome-stable_current_amd64.deb
-#rm -rf ./google-chrome-stable_current_amd64.deb
-
-#dconf write /org/gnome/desktop/screensaver/lock-enabled false
