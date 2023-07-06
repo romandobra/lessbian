52510a50e2db76722108ce5919d0a1acab737052
romandobra <67537080+romandobra@users.noreply.github.com>
2023-07-06 19:02:23 +0000

    Update x-headless-docker.sh

--- settings/x-headless-docker.sh
+++ settings/x-headless-docker.sh
@@ -1,2 +1,2 @@
 BASE="X"
-PARTS="common-apps power-settings overlay sshd-root-nopassword desktop-settings desktop-apps google-chrome docker-desktop"
+PARTS="common-apps power-settings overlay sshd-root-nopassword autologin-gnome desktop-settings desktop-apps google-chrome docker-desktop"
