decee29ab51d5391001d25059cb378cca4c969d2
Roman Dobra <romanreachless@gmail.com>
2024-08-21 19:57:54 +0000

    rewrite

--- install.sh
+++ install.sh
@@ -1,37 +1,65 @@
 #!/usr/bin/env bash
 
 set -e
-
-apt-get -qq update
-apt-get -yqq install debootstrap
-
+if [ "$1" == clean ]; then rm -rf {part-*.sh,variant.sh}; fi
 (grub-install --help; wget --help; chroot --help) > /dev/null
 
+
 export LESSBIAN_GIT='https://raw.githubusercontent.com/romandobra/lessbian/main'
+get_file(){
+    wget -q -O - "$LESSBIAN_GIT/$1"
+}
+export -f get_file
 
-if [ ! -f settings.sh ]; then
-  echo
-  echo 'File settings.sh not found.'
-  echo 'Download it from (leave empty to exit):'
-  read -p "$LESSBIAN_GIT/settings/***.sh: "
-  if [[ "x$REPLY" == "x" ]]; then exit 1; fi
-  wget -O settings.sh $LESSBIAN_GIT/settings/${REPLY}.sh || exit 2
+
+if [ ! -f variant.sh ]; then
+  IFS=$'\n' read -d '' -r -a variants < <(get_file variants/_list | cut -d'.' -f1 && printf '\0')
+  for (( i=0; i<${#variants[@]}; i++ )); do
+    printf "%2s  %s\n" "$i" "${variants[$i]}"
+  done
+  read -p "Choose variant [0]: " variant
+  export LESSBIAN_VARIANT=${variants[${variant:-0}]}
+  echo "export LESSBIAN_VARIANT=${variants[${variant:-0}]}" > variant.sh
+  get_file "variants/${LESSBIAN_VARIANT}.sh" >> variant.sh
 fi
-source settings.sh
-LESSBIAN_BASE=$BASE
-LESSBIAN_PARTS=$PARTS
+source variant.sh
 
-COUNTER=0
-for i in $LESSBIAN_PARTS; do
-  let "COUNTER+=5"
-  wget -O custom-$(printf "%02d\n" $COUNTER)-${i}.sh $LESSBIAN_GIT/parts/${i}.sh
-done || true
 
-echo "settings.sh and custom-***.sh files are downloaded."
-read -p "Review them if needed and press Enter to install"
+export LESSBIAN_BASE=$BASE
+export LESSBIAN_VARIANT_PARTS=$PARTS
+IFS=' ' read -d '' -r -a LESSBIAN_PARTS < <(echo -n $LESSBIAN_VARIANT_PARTS && printf '\0')
+for (( i=0; i<${#LESSBIAN_PARTS[@]}; i++ )); do
+  file_name="part-$(printf '%02d' ${i})0-${LESSBIAN_PARTS[$i]}.sh"
+  if [ ! -f "$file_name" ]; then
+    get_file "parts/${LESSBIAN_PARTS[$i]}.sh" > "$file_name"
+    else no_review="y"
+  fi
+done
 
-ls -la /dev/disk/by-id; read -p "DEV=/dev/" DEV; DEV="/dev/$DEV"
-read -p "Mount to [/mnt]: " MNT; MNT=${MNT:-/mnt}
+
+if [ "x$no_review" == x ]; then
+  read -p "Review part-* files if needed and press Enter to continue: "
+fi
+
+
+if [ "x$LESSBIAN_INSTALL_DEV" == x ]; then
+  lsblk | grep part
+  read -p "Device to install to (You have to set the boot flag manually): /dev/" LESSBIAN_INSTALL_DEV
+  export LESSBIAN_INSTALL_DEV="/dev/$LESSBIAN_INSTALL_DEV"
+fi
+
+
+if [ "x$LESSBIAN_MOUNT_TARGET" == x ]; then
+  read -p "Mount target [/mnt]: " LESSBIAN_MOUNT_TARGET
+  export LESSBIAN_MOUNT_TARGET=${LESSBIAN_MOUNT_TARGET:-/mnt}
+fi
+
+
+env | grep -E '^LESSBIAN_'
+exit
+
+
+
 
 if [ ! -f ${LESSBIAN_BASE}.tar.gz ]; then wget https://github.com/romandobra/lessbian/releases/download/v2.0/${LESSBIAN_BASE}.tar.gz; fi
 
@@ -48,7 +76,7 @@ mount -t proc /proc $MNT/proc; mount -t sysfs /sys $MNT/sys; mount -B /dev $MNT/
   echo
 } > $MNT/etc/lessbian-env
 
-for i in custom*; do
+for i in part*; do
   cp $i $MNT/s
   chroot $MNT bash /s
   rm $MNT/s
