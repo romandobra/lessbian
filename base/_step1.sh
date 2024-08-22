2c87e93bf6539ad381663844103b606b5bc70f5c
Roman Dobra <romanreachless@gmail.com>
2024-08-22 15:11:21 +0000

    prepare steps

---
+++ base/_step1.sh
@@ -0,0 +1,2 @@
+apt-get -yqq update
+apt-get -yqq install ca-certificates
---
+++ base/_step2.sh
@@ -0,0 +1,4 @@
+apt-get -yqq update
+apt-get -yqq install locales
+sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+dpkg-reconfigure --frontend=noninteractive locales
