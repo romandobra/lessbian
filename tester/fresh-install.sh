006ed15ae5e0fac0ad7bb4471664e540fdca8792
Roman Dobra <romanreachless@gmail.com>
2024-08-23 10:07:10 +0000

    initial

---
+++ tester/fresh-install.sh
@@ -0,0 +1,23 @@
+fdisk /dev/sda
+mkfs.ext4 /dev/sda1
+
+export DEBCONF_FRONTEND=noninteractive
+apt-get -yqq install debootrstrap grub2
+
+sleep 1
+export LESSBIAN_DEBIAN_RELEASE=buster
+export LESSBIAN_VARIANT=x-0
+#export LESSBIAN_JUST_BASE=y
+export LESSBIAN_SKIP_REVIEW=y
+export LESSBIAN_INSTALL_DEV=sda1
+export LESSBIAN_MOUNT_TARGET=/mnt
+export LESSBIAN_HOSTNAME=lessbian-tester
+export LESSBIAN_BASE_FRESH=y
+
+wget https://raw.githubusercontent.com/romandobra/lessbian/main/install.sh
+
+sleep 1
+bash install.sh
+
+sleep 1
+reboot
