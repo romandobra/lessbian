bc8c67fa39ef181a3ef582c438d7efa6368c6383
Roman Dobra <romanreachless@gmail.com>
2024-08-24 12:17:09 +0000

    chrome fix

--- base/_step1.sh
+++ base/_step1.sh
@@ -1,2 +1,3 @@
 apt-get -yqq update
+apt-get -yqq install software-properties-common
 apt-get -yqq install ca-certificates
--- parts/google-chrome.sh
+++ parts/google-chrome.sh
@@ -1,8 +1,18 @@
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main'\
+    | tee /etc/apt/sources.list.d/google-chrome.list
+
+apt-get update
+apt-get install google-chrome-stable
+
+read
+
 chmod -x $(type -p gnome-keyring-daemon)
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
 wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 dpkg -i google-chrome-stable_current_amd64.deb || true
-apt-get --no-install-recommends -y install -f
+apt-get -y install -f
 
 dpkg -i google-chrome-stable_current_amd64.deb || true
 
