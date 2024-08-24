83a2600c62fe576fcb37fcaa2567b74d23817d2e
Roman Dobra <romanreachless@gmail.com>
2024-08-24 08:42:12 +0000

    todo

--- parts/desktop-settings.sh
+++ parts/desktop-settings.sh
@@ -1,4 +1,6 @@
-wget -qO /home/user/dconf $LESSBIAN_GITHUB_URL/parts/dconf
-chown user /home/user/dconf
-runuser -c 'cat /home/user/dconf | dconf load /' user
-rm /home/user/dconf
+# TODO install without X11
+# Cannot autolaunch D-Bus without X11 $DISPLAY
+# wget -qO /home/user/dconf $LESSBIAN_GITHUB_URL/parts/dconf
+# chown user /home/user/dconf
+# runuser -c 'cat /home/user/dconf | dconf load /' user
+# rm /home/user/dconf
